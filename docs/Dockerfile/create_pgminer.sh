cfc83052a60558668722e6fd222027b83f050090
Fausto Ferreira Junqueira <fausto@notebook.fausto.com>
2015-11-29 11:14:47 -0200

    Fineshed service dockerfile and clear object file trash

--- docs/Dockerfile/create_pgminer.sh
+++ docs/Dockerfile/create_pgminer.sh
@@ -1,5 +1,8 @@
-docker run -d \
+#!/bin/bash
+
+sudo docker run -d \
   -p 5432:5432 \
+  -e POSTGRES_PASSWORD=postgresql \
   -v pgminer/psqldata/:/var/lib/postgresql/data \
-  -v pgminer/psqllib/:/usr/lib/postgresql/$PG_MAJOR/lib \
-  faustofjunqueira/pgminer 
+  -v pgminer/psqllib/:/usr/lib/postgresql/9.3/lib \
+  faustofjunqueira/pgminer
