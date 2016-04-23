72986c35edc2f59c64e096c7c622e39d058e5e02
Fausto Junqueira <faustoferjunqueira@gmail.com>
2016-04-22 23:58:46 -0300

    Remove all

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,3 @@
  * Created by fausto on 4/21/16.
  */
 
-var gulp = require('gulp');
-var less = require('gulp-less');
-
-
-gulp.task('less', function(){
-  return gulp.src('less/agency.less')
-    .pipe(less({
-      paths: ['less/mixins.less', 'less/variables.less']
-    }))
-    .pipe(gulp.dest('./css'));
-});
-
-gulp.task('default',function(){
-
-  gulp.watch('less/*', ['less']);
-});
